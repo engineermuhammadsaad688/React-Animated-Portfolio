d8b6ab6dfc482a851cf7be5568d79e1a7a0a3932
muhammad saad <engineer.muhammadsaad688@gmail.com>
2025-09-28 17:10:09 +0500

    React-Animated-Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Home />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className="container home">
+      <div className="left">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, minus!</h1>
+       <a href="" className="btn btn-outline-warning">Download Resume</a>
+      </div>
+      <div className="right"></div>
+    </div>
+    </>
+  )
+}
+
+export default Home
