09f537a3a448ba85415e2c9fdb77d178891974a6
muhammad saad <engineer.muhammadsaad688@gmail.com>
2025-09-29 14:38:47 +0500

    React-Animated-Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Experience from './components/Experience'
+
 const App = () => {
   return (
     <>
       <Navbar />
       <div className="container">
         <Home />
+        <Experience/>
       </div>
 
     </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-
+import pdf from '../pdf/resume.pdf'
+import hero from './data/hero.json'
 const Home = () => {
+  console.log('saad',hero)
   return (
     <>
-    <div className="container home">
-      <div className="left">
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, minus!</h1>
-       <a href="" className="btn btn-outline-warning">Download Resume</a>
+      <div className="container home">
+        <div className="left">
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <a href={pdf} download="Resume.pdf" className="btn btn-outline-warning">Download Resume</a>
+        </div>
+        <div className="right">
+          <div className="img">
+            <img src={`/assets/${hero.imgSrc}`} alt="hero" />          
+            </div>
+        </div>
       </div>
-      <div className="right"></div>
-    </div>
     </>
   )
 }
 
-export default Home
+export default Home;
